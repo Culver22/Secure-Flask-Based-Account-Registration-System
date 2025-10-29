94c58a3f7734829ceaa5f8e0401e5400b8b035f2
Culver22 <o.culver1@newcastle.ac.uk>
2025-10-29 13:53:45 +0000

    Created forms.py in order to validate a username and email for task 1 as well as the foundations of password validation and a bio/ submit field for part C

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp, regexp
+
+blacklist = ['admin', 'root', 'superuser']  #
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            # server-side required
+            Length(min=3, max=30, message="Must be 3–30 characters."),
+            # length constraint of 30 max characters
+            Regexp(r"^[A-Za-z_]+$", message="Only letters and underscores allowed."),
+            # Only allows letters (including capitals) and underscores
+        ],
+    )
+    email = StringField(
+        "Email",
+        validators=[
+            DataRequired(),
+            # server-side required
+            Regexp( r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(?:edu|ac\.uk|org)$'),
+            # always confirms the correct email format, also makes sure of an @ and the allowed domains
+        ]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(), EqualTo("password", message="Passwords must match.")],
+    )
+
+    bio = TextAreaField("Bio / Comment")
+    submit = SubmitField("Register")
