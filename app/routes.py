cb5da43aade172dda4094268bd4795ccb0c0409e
Culver22 <o.culver1@newcastle.ac.uk>
2025-11-02 17:48:26 +0000

    Updates requirements.txt and also added a client_ip method, to get the real IP

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,15 @@ whitelisted_tags = ['b', 'i', 'u', 'em', 'strong', 'a', 'p', 'ul', 'ol', 'li']
 whitelisted_attributes = {'a': ['href', 'title']}
 whitelisted_protocols = ['http', 'https', 'mailto']
 
+warning_message = "Some HTML content was removed for safety"
+
 @main.route('/', methods=['GET'])
 def home():
     return redirect(url_for('main.register'))
+
+def client_ip():
+    # Returns real client IP, if behing a proxy use X-forwarded-For first
+    return request.headers.get('X-Forwarded-For', request.remote_addr)
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
