9e4c77d02e6c78883f1258b939859f1c5d8a3c29
Culver22 <o.culver1@newcastle.ac.uk>
2025-11-04 13:18:54 +0000

    added logging with UTC timestamps

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from config import Config
-import os
+import logging
+import time
 
 def create_app():
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__)
     app.config.from_object(Config)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logging.Formatter.converter = time.gmtime # UTC clock
+    handler.setFormatter(formatter)
+    app.logger.setLevel(logging.INFO)
+    if not app.logger.handlers:
+        app.logger.addHandler(handler)
 
     from .routes import main
     app.register_blueprint(main)
-
     return app
